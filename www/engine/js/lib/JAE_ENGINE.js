ba3347eeefb9553b1de1d79dc2a948326cd668ef
victor <kozlov-victor@rambler.ru>
2014-04-17 00:20:36 +0300

    Метатеги

--- www/engine/js/lib/JAE_ENGINE.js
+++ www/engine/js/lib/JAE_ENGINE.js
@@ -1043,8 +1043,11 @@ var ENGINE = (function () {
             var metaTagCharSet = document.createElement('meta');
             var cnv = document.createElement('canvas');
             document.getElementsByTagName('body')[0].appendChild(cnv);
-            window.scrollTo(0,1);
             _external.SceneManager._setContext(cnv);
+            setTimeout(function(){
+                // Hide the address bar!
+                window.scrollTo(0, 1);
+            }, 0);
         }, 50);
 
     }
